1d19253754717be8a06a77797009b418942ef41e
‘Fabrice’ <fabrice.avrila@gmail.com>
2021-09-16 18:27:05 +0200

    Erreur 500

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,10 +1,9 @@
-const saucesNew = require("../models/sauces");
+const Sauces = require("../models/sauces");
 
 exports.createSauces = (req, res, next) => {
-  const sauceObject = JSON.parse(req.body.saucesNew);
-  console.log(sauceObject);
+  const sauceObject = JSON.parse(req.body.sauces);
   delete sauceObject._id;
-  const sauces = new saucesNew({
+  const sauces = new Sauces({
     ...sauceObject,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
@@ -17,15 +16,13 @@ exports.createSauces = (req, res, next) => {
 };
 
 exports.getOneSauce = (req, res, next) => {
-  sauces
-    .findOne({ _id: req.params.id })
+  Sauces.findOne({ _id: req.params.id })
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(404).json({ error }));
 };
 
 exports.getAllSauces = (req, res, next) => {
-  sauces
-    .find()
+  Sauces.find()
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
 };
