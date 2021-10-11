41b0343bd4883cb92643dfb8f3d35652de01fb65
‘Fabrice’ <fabrice.avrila@gmail.com>
2021-10-11 15:17:32 +0200

    test

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -49,30 +49,3 @@ exports.login = (req, res, next) => {
     //Mongoose va chercher l'utilisateur, cependant il pourrait y avoir un problème de connection avec 500 pour erreur server
     .catch((error) => res.status(500).json({ error }));
 };
-
-exports.login = (req, res, next) => {
-  //Méthode findOne pour trouver un seul utilisateur de la base de donnée
-  User.findOne({ email: req.body.email }) //L'addresse mail doit correspondre à celle envoyée dans la requête
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-          });
-        })
-        //Mongoose va chercher l'utilisateur, cependant il pourrait y avoir un problème de connection avec 500 pour erreur server
-        .catch((error) => res.status(500).json({ error }));
-    })
-    //Mongoose va chercher l'utilisateur, cependant il pourrait y avoir un problème de connection avec 500 pour erreur server
-    .catch((error) => res.status(500).json({ error }));
-};
